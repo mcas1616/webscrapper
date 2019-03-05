d38879055a03de9178615e81968ab2d7292f41d8
jun <mcas1616@gmail.com>
2019-03-05 17:12:44 +0000

    add PageContents

---
+++ src/components/PageContents.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Grid from "@material-ui/core/Grid";
+import Paper from "@material-ui/core/Paper";
+
+const styles = theme => ({
+    root: {
+        flexGrow: 1,
+        boxShadow: '0px 1px 5px 0px rgba(0,0,0,0.2), 0px 2px 2px 0px rgba(0,0,0,0.14), 0px 3px 1px -2px rgba(0,0,0,0.12)',
+        margin: '50px 0',
+        backgroundColor: 'white'
+    },
+    paper: {
+        padding: theme.spacing.unit * 2,
+        color: theme.palette.text.secondary,
+        boxShadow: 'none',
+    },
+    pageContent: {
+        overflow:'auto',
+        border: '1px solid black',
+        zoom: '0.75',
+    },
+});
+
+class PageContents extends React.Component{
+    constructor(props){
+        super(props);
+    }
+
+    createMarkup = (pageResult) => {
+        return {__html: pageResult};
+    }
+
+    render() {
+        const { classes } = this.props;
+        console.log('PageContents render()', this.props);
+        return (
+            <div>
+                <div className={classes.root}>
+                    <Grid container spacing={24}>
+                        <Grid item xs={8}>
+                            <Paper className={classes.paper}>
+                                {this.props.pageResult == '' ? '' : <div className={classes.pageContent} dangerouslySetInnerHTML={this.createMarkup(this.props.pageResult)}></div>}
+                            </Paper>
+                        </Grid>
+                    </Grid>
+                </div>
+            </div>
+        );
+    }
+}
+
+PageContents.propTypes = {
+    classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(PageContents);
--- src/containers/mainPage/index.js
+++ src/containers/mainPage/index.js
@@ -2,23 +2,20 @@ import React from 'react';
 import { connect } from 'react-redux';
 import * as actions from '../../actions';
 import InputUrl from "../../components/InputUrl";
+import PageContents from "../../components/PageContents";
 
 class MainPage extends React.Component{
     constructor(props){
         super(props);
     }
 
-    createMarkup = () => {
-        return {__html: this.props.pageResult};
-    }
 
     render() {
         console.log(this.props);
         return(
             <div>
-                <InputUrl getPageResult={this.props.getPageResult}/>
-                <div dangerouslySetInnerHTML={this.createMarkup()}>
-                </div>
+                <InputUrl getPageResult={this.props.getPageResult} />
+                <PageContents pageResult={this.props.pageResult}/>
             </div>
         );
     }
